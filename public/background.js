e4108135d7096cd8fc6bdbe2424e4866ec60ce4f
yad <yad@yad.com>
2018-01-26 21:06:44 +0100

    fix: some fix

--- public/background.js
+++ public/background.js
@@ -1,7 +1,12 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {          
-    if (changeInfo.status == 'complete') {   
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: "start" });
+            const getState = () => {
+                var state = localStorage.getItem("formState");
+                return state ? JSON.parse(state) : {enabled: true};
+            }
+
+            chrome.tabs.sendMessage(tabs[0].id, { type: "refresh", value: getState() });
         });
     }
 });
--- public/content.js
+++ public/content.js
@@ -1,4 +1,5 @@
 (function() {
+    let run = () => {};
     $(function() {
         const url = window.location.href;
         const vstsPattern = ".visualstudio.com";
@@ -13,58 +14,58 @@
             }
             injectScript( chrome.extension.getURL('inject.js'), 'body');
 
-            const interval = 1000;
-
-            const getState = () => {
-                var state = localStorage.getItem(formSaveKey);
-                return state ? JSON.parse(state) : {enabled: true};
-            }
-
-            const run = () => {
-                internalRun(getState);
-                setInterval(() => internalRun(getState), interval);
+            let interval = null;
+            run = (state) => {
+                internalRun(state);
+                if (interval) {
+                    clearInterval(interval);
+                }
+                interval = setInterval(() => internalRun(state), 1000);
             };
 
-            const internalRun = (getState) => {
-                const state = getState();
-                console.log(locateElement("body"));
+            const internalRun = (state) => {
                 if (!state.enabled) {
                     return;
                 }
-                            
+
                 if (!url.includes(state.vsts || "")) {
                     return;
                 }
-            
+
+                console.log($("div.vc-pullrequest-leftpane-section"));
+
+                const officialModule = $("div.vc-pullrequest-leftpane-section").has($("div.vc-pullrequest-workitem-container"));
+                officialModule.hide();
+
                 const description = locateElement("div.vc-pullrequest-activity-feed-description");
                 const missingWorkItemLink = !description.text().includes(state.externalWorkItem);
-            
-                const complete = locateElement("button#pull-request-complete-button");
+
+                const complete = locateElement("div.vc-pullrequest-action-bar");
                 const listPolicies = locateElement("div.vc-pullrequest-merge-policy-status-list div.ms-List-surface");
-            
+
                 refreshExternalWorkItemPolicy(listPolicies, complete, missingWorkItemLink);
             }
-            
+
             const locateElement = (selector) => {
                 const element = $(selector);
                 if (!element) {
                     throw new Error(`VSTS not found: ${selector}`);
                 }
-            
+
                 return element;
             }
-            
+
             const externalWorkItemHook = "external-workitem-hook";
             const refreshExternalWorkItemPolicy = (listPolicies, complete, error) => {
-                const label = error ? "Missing workitem reference" : "Workitem added";
+                const label = error ? "Work item is missing" : "Work item added";
                 const errorOrSuccessClass = `bowtie-${error ? "math-multiply" : "check"}`;
-            
+
                 if (error) {
                     complete.hide();
                 } else {
                     complete.show();
-                }       
-            
+                }
+
                 const alreadyExists =$(`div#${externalWorkItemHook}`);
                 if (!alreadyExists) {
                     listPolicies.append(makePolicy(errorOrSuccessClass, label));
@@ -75,22 +76,21 @@
                     // freeze !
                 }
             };
-            
-            const makePolicy = (errorOrSuccessClass, label) => 
+
+            const makePolicy = (errorOrSuccessClass, label) =>
                 $(`<div id="${externalWorkItemHook}" ${errorOrSuccessClass} role="presentation" class="ms-List-cell" data-list-index="0" data-automationid="ListCell">` +
                     '<div role="listitem" class="vc-pullrequest-merge-policy-status-item">' +
                         `<i class="bowtie-icon ${errorOrSuccessClass}" aria-label="${label}"></i>` +
                         `<span class="statusText">${label}</span>` +
                     '</div>' +
-                '</div>');            
+                '</div>');
 
         }
     });
-    let width = 200;
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if(request.type === "start") {
+        if(request.type === "refresh") {
             $(function() {
-                run();
+                run(request.value);
             });
         }
     });
